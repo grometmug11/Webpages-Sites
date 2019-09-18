874f3a797132fcb7a0b9650693b5126fa9e4a487
MercersKitchen <Mark.Mercer@epsb.ca>
2019-09-18 13:30:42 -0600

    Update

--- JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/VariablesJSON.js
+++ JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/VariablesJSON.js
@@ -1,2 +1,44 @@
-console.log("Did you change my file name?");
-//console.log("You made it to the console!");
+//console.log("Did you change my file name?");
+console.log("You made it to the console!");
+
+console.log(''); // Prints 2 spaces in the Conosle
+console.log(''); // So output is easier to read
+
+// Create 10 variables about yourself and a friend
+// Object Template
+/*
+let person1{
+  firstName =
+  lastName = ;
+  age = ; //Not my real age :)
+  favoriteNumber = ; //security feature
+  hair = ; // explore: undefined, null; origonally "none"
+  // debug with typeof
+  male = ;
+  female = ;
+  other = ;
+}
+*/
+
+let person1{
+  firstName =
+  lastName = ;
+  age = ; //Not my real age :)
+  favoriteNumber = ; //security feature
+  hair = ; // explore: undefined, null; origonally "none"
+  // debug with typeof
+  male = ;
+  female = ;
+  other = ;
+}
+let person2{
+  firstName =
+  lastName = ;
+  age = ; //Not my real age :)
+  favoriteNumber = ; //security feature
+  hair = ; // explore: undefined, null; origonally "none"
+  // debug with typeof
+  male = ;
+  female = ;
+  other = ;
+}
