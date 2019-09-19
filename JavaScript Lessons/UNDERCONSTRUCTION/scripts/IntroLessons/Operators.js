b025739fc9022ac8b873a7d3bacd5688c87de6ef
MercersKitchen <Mark.Mercer@epsb.ca>
2019-09-19 13:44:30 -0600

    Update

--- JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/Operators.js
+++ JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/Operators.js
@@ -1,2 +1,26 @@
 //console.log("Did you change my file name?");
 console.log("You made it to the console!");
+
+function simpleOperators (numberA, numberB) {
+  let adding = numberA + numberB;
+  let subtraction = numberA - numberB;
+  let mulitplying = numberA * numberB;
+  let division = numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction', subtraction);
+  console.log(numberA, numberB, 'mulitplying', mulitplying);
+  console.log(numberA, numberB, 'division', division);
+  console.log(numberA, numberB, 'modulus', modulus);
+}
+
+skip count by _____ for _____ times starting at _____
+function skipCpinting (interval, times, startingNumber) {
+  
+}
+
+let number = [4, 7, 13, 99, 0, 100, 5, 1, '1'];
+
+simpleOperators(number[0], number[1]);
+simpleOperators(number[2], number[3]);
+simpleOperators(number[4], number[5]);
