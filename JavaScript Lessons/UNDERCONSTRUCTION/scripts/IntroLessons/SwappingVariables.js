4b2bfa5e4bf7d65c7419ecd1119979e4318a4570
MercersKitchen <Mark.Mercer@epsb.ca>
2019-09-24 13:49:17 -0600

    Update

--- JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/SwappingVariables.js
+++ JavaScript Lessons/UNDERCONSTRUCTION/scripts/IntroLessons/SwappingVariables.js
@@ -1,2 +1,22 @@
-console.log("Did you change my file name?");
-//console.log("You made it to the console!");
+//console.log("Did you change my file name?");
+console.log("You made it to the console!");
+
+let markMercer = {
+  first: "Mark",
+  last: "Mercer",
+  age: 13,
+  hairColour: "brown",
+}
+let cluckTheChicken = {
+  first: "Cluck",
+  last: "TheChicken",
+  age: 17,
+  hairColour: null,
+}
+console.log(markMercer);
+console.log(cluckTheChicken);
+let swap = markMercer.last; //Last: Mercer
+markMercer.last = cluckTheChicken.last;
+cluckTheChicken.last = swap;
+console.log(markMercer);
+console.log(cluckTheChicken);
